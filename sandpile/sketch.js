a083b1c73829c1a39f6239689e181a265b45b6b5
b0bA <bremic@gmx.net>
2017-01-20 00:38:35 +0100

    update

--- sandpile/sketch.js
+++ sandpile/sketch.js
@@ -9,23 +9,25 @@ var gridSizeSlider;
 var nrOfSandSlider;
 var canvasSize = {width:640, height:640};
 var colors = {
-  0: [0,0,255],
+  0: [255,255,255],
   1: [0,255,0],
   2: [255,0,0],
-  3: [255,0,255],
-  9: [255,255,255]
+  3: [0,0,255],
 }
 var sandpile = new Array(gridSizeY);
 
 function setup() {
   var canvas = createCanvas(canvasSize.width, canvasSize.height);
+  /*asure that the grid has odd length and height*/
+  if (gridSizeX % 2 == 0) gridSizeX += 1;
+  if (gridSizeY % 2 == 0) gridSizeY += 1;
   for (var row = 0; row < gridSizeY; row++){
 	sandpile[row] = new Array(gridSizeX);
-	for (var col = 0; col < gridSizeX; col++){
-		sandpile[row][col] = 9;
-	}
+	//for (var col = 0; col < gridSizeX; col++){
+	//	sandpile[row][col] = 0;
+	//}
   }
-  sandpile[row/2][col/2] = nrOfSand;
+  sandpile[Math.floor(row/2)][Math.floor(col/2)] = nrOfSand;
 }
 
 function draw() {
@@ -37,11 +39,12 @@ function draw() {
   text("FPS: " + round(frameRate(), 2), 20, 20);
   for (var row = 0; row < gridSizeY; row++){
 	for (var col = 0; col < gridSizeX; col++){
-		rect(col, row, width/gridSizeX, height/gridSizeY);
+		x = sandpile[row][col];
+		noStroke();
+		fill(color(colors[x]));
+		rect(col * width/gridSizeX, row * height/gridSizeY, width/gridSizeX, height/gridSizeY);
 	}
   }
-  console.log(sandpile[row/2][col/2]);
-  console.log(sandpile[27][55]);
   noLoop();
 }
 	
